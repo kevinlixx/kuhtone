91d4390b80895420125a1b30b15f20226422241d
JuanMonta09 <111712967+JuanMonta09@users.noreply.github.com>
2024-05-01 17:49:21 -0500

    actualizacion de inicio en psicologo
    Se creo los apartados de mis citas y mis pacientes y estan conectados a sus nuevos formularios para cada uno de ellos

---
+++ pacientes_psicologo.php
@@ -0,0 +1,71 @@
+<?php
+    include("./config/conexion.php");
+    $id_profesional= $_GET['id_perfil'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kuhtone</title>
+    <script src="https://kit.fontawesome.com/79e6024c63.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Inter:wght@300;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/Pacientes_psicologos.css">
+    <link rel="stylesheet" href="./css/tablet_usr.css" media="screen and (min-width: 600px)"/>
+    <link rel="stylesheet" href="./css/desktop_usr.css" media="screen and (min-width: 800px)"/>
+</head>
+<body>
+    <header>
+        <section class="section_header">
+            <figure class="figure_header"> 
+                <img 
+                src="./img/logo_header.svg" 
+                alt="lgo de kuhtone"
+                />  
+                <figcaption></figcaption> 
+            </figure>
+            <div class="menu menu-header">
+                
+                    <figure id="btn_menu">
+                        <img src="./img/menu.svg" alt="menu"/>  
+                        <figcaption></figcaption> 
+                    </figure>                
+                <div id="back_menu"></div>
+                <nav id="nav" class="menu-section">
+                    <img src="img/logo_header.svG" alt="">
+                    <ul> 
+                    <?php
+                      echo'
+                      <li><a href="./index_psicologos.php?id_perfil='.$id_profesional.'">Inicio</a></li>
+                        <li><a href="./queries/consultar_dispo.php?id_perfil='.$id_profesional.'">Mi disponibilidad</a></li>
+                        <li><a href="./perfil_psicologo.php?id_perfil='.$id_profesional.'">Mi perfil</a></li>
+                        <li><a href="./index.php" id="selected">Cerrar Sesion</a></li>';
+                        ?>
+                        
+                    </ul>
+                </nav>
+            </div>
+            <!-- <a href="" class="menu-header">
+            <figure >
+                <img 
+                src="./img/menu.svg" 
+                alt="menu"
+                />  
+                <figcaption></figcaption> 
+            </figure> -->
+        </a>
+    </header>
+
+    <h1 id="titulo-citas">Mis Pacientes</h1> <!-- Título con id -->
+    
+
+    <footer class="pie-pagina">
+    <div class="footer_copy">
+        <small>&copy; 2023 <b>kuhtone</b> - Todos los Derechos Reservados.</small>
+    </div>
+    </footer>
+    <script src="js/script.js"></script>
+</body>
