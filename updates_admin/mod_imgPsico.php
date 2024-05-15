49fee4c66ddfa9ccd335a5b9b8f53c1699cf4ec8
DdyYazo <alejoyaba@gmail.com>
2024-05-14 20:40:28 -0500

    eliminación de imagenes y comprobación de rutas

--- updates_admin/mod_imgPsico.php
+++ updates_admin/mod_imgPsico.php
@@ -11,7 +11,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/Style_imgPerfil.css">
+    <link rel="stylesheet" href="../css/style_imgPerfil.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <script src="https://kit.fontawesome.com/e1d55cc160.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../css/desktop_perfil.css" media="screen and (min-width: 800px)"/>
