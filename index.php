066377931c39c116247e6521139fac2116fc2e4b
DdyYazo <alejoyaba@gmail.com>
2024-05-24 16:23:30 -0500

    Update: Nueva estructura y diseño del index en modo desktop y responsive

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,30 +11,28 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Inter:wght@300;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./css/style.css">
-    <link rel="stylesheet" href="./css/tablet.css" media="screen and (min-width: 600px)"/>
-    <link rel="stylesheet" href="./css/desktop.css" media="screen and (min-width: 800px)"/>
+    <link rel="stylesheet" href="./css/tablet.css" media="screen and (min-width: 600px)" />
+    <link rel="stylesheet" href="./css/desktop.css" media="screen and (min-width: 800px)" />
 </head>
+
 <body>
     <header>
         <section class="section_header">
-            <figure class="figure_header"> 
-                <img 
-                src="./img/logo_header.svg" 
-                alt="lgo de kuhtone"
-                />  
-                <figcaptio></figcaptio n> 
+            <figure class="figure_header">
+                <img src="./img/logo_header.svg" alt="lgo de kuhtone" />
+                <figcaptio></figcaptio n>
             </figure>
             <div class="menu menu-header">
-                
-                    <figure id="btn_menu">
-                        <img src="./img/menu.svg" alt="menu"/>  
-                        <figcaption></figcaption> 
-                    </figure>                
+
+                <figure id="btn_menu">
+                    <img src="./img/menu.svg" alt="menu" />
+                    <figcaption></figcaption>
+                </figure>
                 <div id="back_menu"></div>
                 <nav id="nav" class="menu-section">
                     <img src="img/logo_header.svg" alt="">
-                    <ul> 
-                        <li><a href="./index.php" >Inicio</a></li>
+                    <ul>
+                        <li><a href="./index.php">Inicio</a></li>
                         <li><a href="./login-register.php" id="selected">Iniciar Sesion</a></li>
                     </ul>
                 </nav>
@@ -46,99 +45,96 @@
                 />  
                 <figcaption></figcaption> 
             </figure> -->
-        </a>
+            </a>
     </header>
     <main>
-            <div class="container">
-                <!-- <div class="cover"> -->
+        <!-- Intro de la pagina -->
+        <div class="div-info-container" id="intro">
+            <div class="row--right">
+                <div class="col-md-6">
                     <div class="text">
-                        <h1>¡Agenda tus citas en consultorios virtuales de manera rápida y sencilla con nuestro aplicativo web!</h1>
-                       
+                        <h1 class="text-colored">¡Agenda tus citas de manera presencial o virtual de forma rápida y sencilla con nuestro aplicativo!</h1>
                         <p> Logra una mejor salud y bienestar: ¡Agenda tus citas médicas en línea con nuestro aplicativo web de consultorios virtuales y recibe atención médica de calidad desde la comodidad de tu hogar!
                         </p>
                         <a href="./login-register.php">
-                        <input type="button" value="Empieza Ya!">
+                            <input type="button" value="Empieza Ya!">
                         </a>
                     </div>
-                    <div class="image_svg">
-                        <img src="img/woman-choosing_.svg" alt="seleccion cita"> <!--muestra el fondo simbolico/-->
-                    </div>
-                <!-- </div> -->
-            </div>
-            <section class="container_cov"></section>
-            <div class="cover">
-                <div class="psyco_svg">
-                    <img src="img/woman_booking.svg" alt="consultorio">
                 </div>
+                <div class="col-md-6">
+                    <div class="intro_svg">
+                        <img src="img/intro_psico_pa.svg" alt="seleccion cita">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Seccion de especialidades -->
+        <!-- <section class="container_cov"></section> -->
+        <div class="div-info-container" id="options">
+            <div class="row--left">
+                <div class="col-md-6">
                     <div class="text">
                         <h2>La asistencia que necesitas está disponible aquí</h2>
                         <div class="opcs">
-                            <section>
-                                    <figure class="perfil--logos"> 
-                                         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-video" width="52" height="52" viewBox="0 0 24 24" stroke-width="2" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                            <path d="M15 10l4.553 -2.276a1 1 0 0 1 1.447 .894v6.764a1 1 0 0 1 -1.447 .894l-4.553 -2.276v-4z" />
-                                            <path d="M3 6m0 2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2z" />
-                                          </svg> 
-                                        <figcaption></figcaption> 
-                                    </figure>
-                                </section>
                             <p>
-                                LLeva la terapia contigo siempre que la necesites y desde cualquier ubicación.
+                                <object type="image/svg+xml" data="img/camera.svg" class="icon icon-tabler icon-tabler-switch-3" width="52" height="52"></object>
+                                LLeva la terapia contigo siempre que la necesites y a tu comodidad.
                             </p>
-                            <section>
-                                <figure class="perfil--logos"> 
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-users" width="52" height="52" viewBox="0 0 24 24" stroke-width="2" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                        <path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
-                                        <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-                                        <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-                                        <path d="M21 21v-2a4 4 0 0 0 -3 -3.85" />
-                                      </svg>
-                                    <figcaption></figcaption> 
-                                </figure>
-                            </section>   
                             <p>
+                                <object type="image/svg+xml" data="img/peop.svg" class="icon icon-tabler icon-tabler-switch-3" width="52" height="52"></object>
                                 Encuentra la ayuda de psicólogos especializados disponibles para acompañarte.
                             </p>
-                            <section>
-                                <figure class="perfil--logos"> 
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home" width="52" height="52" viewBox="0 0 24 24" stroke-width="2" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                        <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
-                                        <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
-                                        <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
-                                      </svg>
-                                    <figcaption></figcaption> 
-                                </figure>
-                            </section>
                             <p>
-                                Adaptado a tus necesidades y comodidad.
+                                <object type="image/svg+xml" data="img/localt.svg" class="icon icon-tabler icon-tabler-switch-3" width="52" height="52"></object>
+                                Adaptado a tus necesidades desde el punto mas cercano a tu hogar.
                             </p>
-                    </div>
-                    </div>
-
-                    <!-- <section class="psycologs">
-                        <div class="cards">
-                            <h2 class="title">
-                                Encuentra a tu psicologo 
-                            </h2>
-                            <div class="coontent">
-                                <div class="card_psi">
-                                    <div class="content_card">
-
-                                    </div>
-                                </div>
-                                <a href="">Encuentra mas psicologos</a>
-                            </div>
                         </div>
-                    </section> -->
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="image_svg">
+                        <img src="img/psico_intro.svg" alt="consultorio">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Seccion de modalidad virtual -->
+        <div class="div-info-container" id="virtual">
+            <div class="row--right">
+                <div class="col-md-6">
+                    <div class="text">
+                        <h2>Inicia tu terapia desde cualquier ubicación en la que te encuentres con solo tener conexión a internet</h2>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="image_svg">
+                        <img src="img/qr_intro.svg" alt="qrCode">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Seccion de modalidad presencial: Modificar estructura para visualización de mapa con las sedes-->
+        <div class="div-info-container" id="presencial">
+            <div class="row--left">
+                <div class="col-md-6">
+                    <div class="text">
+                        <h2>Si prefieres la atención presencial, también puedes agendar tu cita en sede del consultorio de tu preferencia</h2>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="image_svg">
+                        <img src="img/psico_intro.svg" alt="consultorio">
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
-   <footer class="pie-pagina">
-   <div class="footer_copy">
-        <small>&copy; 2023 <b>kuhtone</b> - Todos los Derechos Reservados.</small>
-   </div>
-</footer>
-<script src="js/script.js"></script>
+    <footer class="pie-pagina">
+        <div class="footer_copy">
+            <small>&copy; 2023 <b>kuhtone</b> - Todos los Derechos Reservados.</small>
+        </div>
+    </footer>
+    <script src="js/script.js"></script>
 </body>
+
 </html>
