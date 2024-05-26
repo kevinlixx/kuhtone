82f5a49d87eac7d96a3f91539cb4d93bb232b578
DdyYazo <alejoyaba@gmail.com>
2024-05-26 16:47:26 -0500

    Update: enlace de estilo de tablet en Citas_psicologo corregido y campos nuevos de fecha y hora de cita agregados

--- Citas_psicologo.php
+++ Citas_psicologo.php
@@ -16,7 +16,7 @@ $fecha_busqueda = isset($_GET['fecha']) ? $_GET['fecha'] : null;
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Inter:wght@300;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./css/style_mis_citas.css">
-    <link rel="stylesheet" href="./css/tablet_usr.css" media="screen and (min-width: 600px)" />
+    <!-- <link rel="stylesheet" href="./css/tablet_usr.css" media="screen and (min-width: 600px)" /> -->
     <link rel="stylesheet" href="./css/desktop_mis_citas.css" media="screen and (min-width: 800px)" />
 </head>
 
@@ -76,10 +76,10 @@ $fecha_busqueda = isset($_GET['fecha']) ? $_GET['fecha'] : null;
                 include("./config/conexion.php");
 
                 if ($fecha_busqueda) {
-                    $consulta = mysqli_query($conection, "SELECT paciente.* FROM paciente 
-                                                          JOIN agendamiento ON paciente.id_paciente = agendamiento.id_paciente 
-                                                          JOIN disponibilidad ON agendamiento.id_disponibilidad = disponibilidad.id_disponibilidad 
-                                                          WHERE disponibilidad.id_profesional = $id_profesional AND disponibilidad.fecha_disponibilidad = '$fecha_busqueda'")
+                    $consulta = mysqli_query($conection, "SELECT paciente.*, disponibilidad.fecha_disponibilidad, disponibilidad.hora_inicio, disponibilidad.hora_final FROM paciente
+                                                      JOIN agendamiento ON paciente.id_paciente = agendamiento.id_paciente
+                                                      JOIN disponibilidad ON agendamiento.id_disponibilidad = disponibilidad.id_disponibilidad
+                                                      WHERE disponibilidad.id_profesional = $id_profesional AND disponibilidad.fecha_disponibilidad = '$fecha_busqueda'")
                         or die("Error al traer los datos");
 
                     if (mysqli_num_rows($consulta) > 0) {
@@ -100,6 +100,9 @@ $fecha_busqueda = isset($_GET['fecha']) ? $_GET['fecha'] : null;
                                     <p><span class="item--aco">Apellidos:</span> ' . $consulta_total["apellidos"] . '</p>
                                     <p><span class="item--aco">Correo:</span> ' . $consulta_total["correo"] . '</p>
                                     <p><span class="item--aco">Teléfono móvil:</span> ' . $consulta_total["telefono_movil"] . '</p>
+                                    <p><span class="item--aco">Fecha de la cita:</span> ' . $consulta_total["fecha_disponibilidad"] . '</p>
+                                    <p><span class="item--aco">Hora de inicio:</span> ' . $consulta_total["hora_inicio"] . '</p>
+                                    <p><span class="item--aco">Hora de finalización:</span> ' . $consulta_total["hora_final"] . '</p>
                                 </div>
                                 <div class="mas_info--description-container">                              
                                 <a href="info_paciente.php?id_paciente=' . $consulta_total["id_paciente"] . '&id_perfil=' . $id_profesional . '" class="mas_info--description">
